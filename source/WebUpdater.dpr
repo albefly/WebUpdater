4fe8affa3803422907434c3b15cb821c676934e1
Alex Kononov <akononov@internationalcybernetics.com>
2019-06-18 20:45:34 -0400

    [+] Initial

---
+++ source/WebUpdater.dpr
@@ -0,0 +1,14 @@
+program WebUpdater;
+
+uses
+  Vcl.Forms,
+  MainForm in 'Forms\MainForm.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
